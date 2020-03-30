c9ec8d1b90561b20cb0c9ec01eb9c485b85f3d61
ESJiang <43910771+ESJiang@users.noreply.github.com>
2020-03-31 05:54:27 +1100

    Update and rename message.php to judge.php

---
+++ judge.php
@@ -0,0 +1,21 @@
+
+<?php
+echo 'username: ', $_POST['username'];
+echo '<br>';
+echo 'password: ', $_POST['password'];
+echo '<br>';
+echo  date('Y-m-d', time());
+echo '<br>';
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+{
+    echo $_SERVER['HTTP_CLIENT_IP'];
+}
+else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+{
+    echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+else
+{
+    echo $_SERVER['REMOTE_ADDR'];
+}
+?>
--- message.php
+++
@@ -1,2 +0,0 @@
-Hi bot! i just wanted to say:</br>
-<!-- send a message using the msg parameter -->
